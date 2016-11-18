bc85bc5e7a10ca0658b5cd761b51a14cec5512d1
Nate Kong <natewkong@gmail.com>
2016-11-18 11:52:47 -0800

    created QoS and message

--- src/entity/packet.java
+++ src/entity/packet.java
@@ -1,6 +1,8 @@
 package entity;
 
 /**
+ * A packet sent by between entities and the network
+ * e.g. IoT device sends data to 
  * 
  * @author Nathan Kong
  * @since Nov 2017
@@ -9,12 +11,22 @@ package entity;
 
 public class packet {
 	private QoS qos;
+	private String message;
 
-	packet(QoS qos) {
+	packet(QoS qos, String message) {
 		this.qos = qos;
+		this.message = message;
 	}
 
 	public QoS getQoS() {
 		return qos;
+	}
+	
+	public String getMessage(){
+		return message;
+	}
+	
+	public void setMessage(String message){
+		this.message = message;
 	}
 }
