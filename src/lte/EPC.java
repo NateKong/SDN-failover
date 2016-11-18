9a66c6daf5c2d88df8647827d840f62020493586
Nate Kong <natewkong@gmail.com>
2016-11-18 11:51:03 -0800

    new entity first commit

---
+++ src/lte/EPC.java
@@ -0,0 +1,15 @@
+package lte;
+
+/**
+ * Evolved Packet Core in LTE system
+ * This system involves the communication
+ * through transmission of packets 
+ * to/from UEs to the internet
+ * 
+ * @author Nathan Kong
+ * @since Nov 2017
+ *
+ */
+public class EPC {
+
+}
