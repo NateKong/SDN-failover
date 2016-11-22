c90a194c7e7fd2828d5c4cb596108cef06f5b4d6
Nate Kong <natewkong@gmail.com>
2016-11-22 07:41:08 -0800

    added comments

--- src/communication/Message.java
+++ src/communication/Message.java
@@ -1,26 +1,16 @@
 package communication;
 
-import entity.QoS;
-
 /**
- * A packet sent by between entities and the network
- * e.g. IoT device sends data to 
+ * A generic message sent by between entities and the network.
+ * This can include transmissions for control or messages to 
+ * other UEs.
+ * e.g. IoT device send to enodeB, enodeb sends to controller
  * 
  * @author Nathan Kong
  * @since Nov 2017
  *
  */
 
-//****************************************************
-// This will need to be renamed we will need to figure
-// out how we want to write the OSI layers
-// QoS happens on layer 3
-//****************************************************
-/*
- * NOTES
- * LAYER 2: Datagram -> contains message(required), destination mac address (not required), and return mac address (required)
- * LAYER 3: Packet -> contains Datagram(required), destination ip addresss (required), and return ip address (required)
- */
 
 public class Message {
 	private QoS qos;
