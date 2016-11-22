40b8d4efde347f82b0a59128a1788511a6e3f1d1
Nate Kong <natewkong@gmail.com>
2016-11-22 07:42:49 -0800

    added comments

--- src/entity/UE.java
+++ src/entity/UE.java
@@ -4,19 +4,29 @@ import sdn.OvEnodeB;
 
 import java.awt.Point;
 
+import communication.QoS;
+
+/**
+ * User Equipment
+ * 
+ * @author Gary Su
+ * @since Nov 2017
+ *
+ */
 public class UE {
 	private OvEnodeB parent;
-	
-	public UE(OvEnodeB tower){
+
+	public UE(OvEnodeB tower) {
 		this.parent = tower;
 		tower.addUE(this);
 	}
 
-	//Sends request to its parent
+	// Sends request to its parent
 	public String sendRequest(QoS type, int bandWeight) {
-		//parent.notify
-		//Above method will notify controller the type of request that is sent out
-		//Then controller will handle accordingly
+		// parent.notify
+		// Above method will notify controller the type of request that is sent
+		// out
+		// Then controller will handle accordingly
 		return "";
 	}
 }
