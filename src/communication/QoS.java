94ffec4ead0728cca5bd8229193ec3ba80e4ef2b
Nate Kong <natewkong@gmail.com>
2016-11-22 07:38:20 -0800

    moved to communication

--- src/communication/QoS.java
+++ src/communication/QoS.java
@@ -1,4 +1,4 @@
-package entity;
+package communication;
 
 /**
  * There are four different QoS classes: (celluar_QoS, Jain)
@@ -8,11 +8,15 @@ package entity;
  * Background class - SMS, download of databases, email program which is running in the background.
  * 
  * transmission - notification to elements for instructions
+ * (e.g. UE transmits to EnodeB to say that its here.  EnodeB transmits to other EnodeBs to find route)
+ * 
+ * M denotes this is a mobile device
+ * I denotes this is an IoT device
  * 
  * @author Nathan Kong
  * @since Nov 2017
  *
  */
 public enum QoS {
-	Conversational, Streaming, Interactive, Background, transmission
+	MConversational, MStreaming, MInteractive, MBackground, transmission, IStreaming, IInteractive, IBackground, Itransmission
 }
