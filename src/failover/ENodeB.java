9fb8e499e8b8c7a460707dc7efa16238cd4f2b4d
Nate Kong <natewkong@gmail.com>
2016-12-25 13:31:53 -0800

    add thread-Runnable

--- src/failover/ENodeB.java
+++ src/failover/ENodeB.java
@@ -1,5 +1,25 @@
 package failover;
 
-public class ENodeB {
+/**
+ * An eNodeB in a LTE
+ * architecture. The eNodeB
+ * is controller using SDN.
+ * 
+ * @author Nathan Kong
+ * @since Jan 2017
+ */
 
+import java.util.ArrayList;
+
+public class ENodeB implements Runnable{
+	private ArrayList<Xtwo> connections;
+
+	public ENodeB() {
+
+	}
+	
+	@Override
+	public void run() {
+		
+	}
 }
