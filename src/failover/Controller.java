33d32351e0f6f8f5ff5a63b60fb805da3dd75916
Nate Kong <natewkong@gmail.com>
2016-12-25 13:31:42 -0800

    add thread-Runnable

--- src/failover/Controller.java
+++ src/failover/Controller.java
@@ -1,5 +1,35 @@
 package failover;
 
-public class Controller {
+/**
+ * A Software Defined Network
+ * Controller.
+ * A controller for a LTE network
+ * 
+ * @author Nathan Kong
+ * @since Jan 2017
+ */
+
+import java.util.ArrayList;
+
+public class Controller implements Runnable{
+	private ArrayList<ENodeB> eNodeBs;
+
+	public Controller() {
+		eNodeBs = new ArrayList<ENodeB>();
+	}
+
+	public void addENodeB(ENodeB e) {
+		eNodeBs.add(e);
+	}
+
+	public void removeENodeB(ENodeB e) {
+		eNodeBs.remove(e);
+	}
+
+	@Override
+	public void run() {
+
+		
+	}
 
 }
