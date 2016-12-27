aae52f624572d2b0817888a76481caaf262523c0
Nate Kong <natewkong@gmail.com>
2016-12-26 23:55:26 -0800

    format code and and adjust naming of Entities

--- src/failover/Simulation1.java
+++ src/failover/Simulation1.java
@@ -50,12 +50,13 @@ public class Simulation1 {
 		// print statistics
 		printStats();
 
-		System.out.println("\nSIMULATION COMPLETE");
+		printNewSection();
+		System.out.println("SIMULATION COMPLETE");
 	}
 
 	private static void setup() {
-		printNewSection();
-		System.out.println("SETUP\n");
+		//printNewSection();
+		//System.out.println("SETUP\n");
 		stats = new ArrayList<String>();
 		controllers = new ArrayList<Controller>();
 		eNodeBs = new ArrayList<ENodeB>();
@@ -70,32 +71,32 @@ public class Simulation1 {
 		System.out.println("INITIALIZE SYSTEM\n");
 
 		/* Create Controllers */
-		System.out.println("Create controllers");
+		System.out.println("Create Controllers");
 		for (int i = 0; i < numOfControllers; i++) {
 			int load = 20;
 			Controller c = new Controller(i, load, startTime, maxTime, stats);
 			controllers.add(c);
-			System.out.println("Controller " + c.getName() + " is created");
+			System.out.println(c.getName() + " is created");
 		}
 
 		/* Create eNodeBs */
 		System.out.println("\nCreate eNodeBs");
-		for (int i = 0, j=0; i < numOfeNodeBs; i++) {
+		for (int i = 0, j = 0; i < numOfeNodeBs; i++) {
 			ENodeB B = new ENodeB(i, startTime, maxTime, stats);
 			eNodeBs.add(B);
-			System.out.println("eNodeB " + B.getName() + " is created");
-			
+			System.out.println(B.getName() + " is created");
+
 			Controller C = controllers.get(j);
 			C.addENodeB(B);
-			System.out.println("Controller" + C.getName() + " adopts eNodeB" + B.getName());
-			if(i%3 == 2) {
+			System.out.println(C.getName() + " adopts " + B.getName());
+			if (i % 3 == 2) {
 				j++;
 			}
-			
+
 		}
 
 		/* Creates connections between ENodeBs */
-		System.out.println("\nCreate connections");
+		System.out.println("\nCreate Connections");
 
 		for (int i = 1; i < numOfeNodeBs; i++) {
 			int bw = 20;
@@ -103,15 +104,13 @@ public class Simulation1 {
 			// creates a connection between the eNodeB and the previously
 			// created one
 			new Xtwo(eNodeBs.get(i - 1), eNodeBs.get(i), bw);
-			System.out.println(
-					"eNodeB" + eNodeBs.get(i - 1).getName() + " is connected to eNodeB" + eNodeBs.get(i).getName());
+			System.out.println(eNodeBs.get(i - 1).getName() + " has a X2 connected to " + eNodeBs.get(i).getName());
 
 			// if this is the third eNodeB in the set
 			// e.g. eNodeB 2, 5, and 8
 			if (i % 3 == 2) {
 				new Xtwo(eNodeBs.get(i - 2), eNodeBs.get(i), bw);
-				System.out.println(
-						"eNodeB" + eNodeBs.get(i - 2).getName() + " is connected to eNodeB" + eNodeBs.get(i).getName());
+				System.out.println(eNodeBs.get(i - 2).getName() + " has a X2 connected to " + eNodeBs.get(i).getName());
 			}
 		}
 
@@ -124,22 +123,23 @@ public class Simulation1 {
 		System.out.println("RUN SIMULATION\n");
 
 		// start components
-		for ( Controller c : controllers) {
+		for (Controller c : controllers) {
 			Thread t = new Thread(c);
 			threads.add(t);
 			t.start();
 
 		}
-		
-		//  joins all the threads and ensures the Main program doesn't continue until the simulation is completed.
-		for (Thread t: threads) {
+
+		// joins all the threads and ensures the Main program doesn't continue
+		// until the simulation is completed.
+		for (Thread t : threads) {
 			try {
 				t.join();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-	
+
 		//
 		System.out.println("finished main");
 	}
