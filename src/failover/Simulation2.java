dd272add522a3b15cab05cc72b93707337a04955
Nate Kong <natewkong@gmail.com>
2017-03-23 05:46:04 -0700

    update simulation time

--- src/failover/Simulation2.java
+++ src/failover/Simulation2.java
@@ -99,27 +99,27 @@ public class Simulation2 {
 		/* Create eNodeBs */
 		//System.out.println("\nCreate eNodeBs");
 
-		ENodeB B0 = new ENodeB(0, maxTime, load);
+		ENodeB B0 = new ENodeB(0, maxTime, load, 175, 1);
 		eNodeBs.add(B0);
-		ENodeB B1 = new ENodeB(1, maxTime, load);
+		ENodeB B1 = new ENodeB(1, maxTime, load, 150, 2);
 		eNodeBs.add(B1);
-		ENodeB B2 = new ENodeB(2, maxTime, load);
+		ENodeB B2 = new ENodeB(2, maxTime, load, 125, 3);
 		eNodeBs.add(B2);
-		ENodeB B3 = new ENodeB(3, maxTime, load);
+		ENodeB B3 = new ENodeB(3, maxTime, load, 100, 4);
 		eNodeBs.add(B3);
-		ENodeB B4 = new ENodeB(4, maxTime, load);
+		ENodeB B4 = new ENodeB(4, maxTime, load, 175, 1);
 		eNodeBs.add(B4);
-		ENodeB B5 = new ENodeB(5, maxTime, load);
+		ENodeB B5 = new ENodeB(5, maxTime, load, 75, 2);
 		eNodeBs.add(B5);
-		ENodeB B6 = new ENodeB(6, maxTime, load);
+		ENodeB B6 = new ENodeB(6, maxTime, load, 75, 3);
 		eNodeBs.add(B6);
-		ENodeB B7 = new ENodeB(7, maxTime, load);
+		ENodeB B7 = new ENodeB(7, maxTime, load, 125, 2);
 		eNodeBs.add(B7);
-		ENodeB B8 = new ENodeB(8, maxTime, load);
+		ENodeB B8 = new ENodeB(8, maxTime, load, 100, 3);
 		eNodeBs.add(B8);
-		ENodeB B9 = new ENodeB(9, maxTime, load);
+		ENodeB B9 = new ENodeB(9, maxTime, load, 100, 4);
 		eNodeBs.add(B9);
-		ENodeB B10 = new ENodeB(10, maxTime, load);
+		ENodeB B10 = new ENodeB(10, maxTime, load, 175, 1);
 		eNodeBs.add(B10);
 
 		/* Create Controllers */
@@ -146,23 +146,23 @@ public class Simulation2 {
 		/* Creates connections between ENodeBs */
 		//System.out.println("\nCreate Connections");
 		
-		Connection x0 = new Connection("C0-E0", controllers.get(0), eNodeBs.get(0));
-		Connection x1 = new Connection("C1-E4", controllers.get(1), eNodeBs.get(4));
-		Connection x2 = new Connection("C2-E10", controllers.get(2), eNodeBs.get(10));
-		Connection x4 = new Connection("E0-E2", eNodeBs.get(0), eNodeBs.get(2));
-		Connection x6 = new Connection("E1-E4", eNodeBs.get(1), eNodeBs.get(4));
-		Connection x7 = new Connection("E2-E5", eNodeBs.get(2), eNodeBs.get(5));
-		Connection x8 = new Connection("E3-E6", eNodeBs.get(3), eNodeBs.get(6));
-		Connection x9 = new Connection("E4-E7", eNodeBs.get(4), eNodeBs.get(7));
-		Connection x10 = new Connection("E5-E8", eNodeBs.get(5), eNodeBs.get(8));
-		Connection x11 = new Connection("E6-E9", eNodeBs.get(6), eNodeBs.get(9));
-		Connection x13 = new Connection("E10-E8", eNodeBs.get(10), eNodeBs.get(8));
-		Connection x15 = new Connection("E1-E2", eNodeBs.get(1), eNodeBs.get(2));
-		Connection x16 = new Connection("E2-E3", eNodeBs.get(2), eNodeBs.get(3));
-		Connection x17 = new Connection("E4-E5", eNodeBs.get(4), eNodeBs.get(5));
-		Connection x18 = new Connection("E5-E6", eNodeBs.get(5), eNodeBs.get(6));
-		Connection x19 = new Connection("E7-E8", eNodeBs.get(7), eNodeBs.get(8));
-		Connection x20 = new Connection("E8-E9", eNodeBs.get(8), eNodeBs.get(9));
+		Connection x0 = new Connection("C0-E0", controllers.get(0), eNodeBs.get(0), 175);
+		Connection x1 = new Connection("C1-E4", controllers.get(1), eNodeBs.get(4), 175);
+		Connection x2 = new Connection("C2-E10", controllers.get(2), eNodeBs.get(10), 175);
+		Connection x4 = new Connection("E0-E2", eNodeBs.get(0), eNodeBs.get(2), 175);
+		Connection x6 = new Connection("E1-E4", eNodeBs.get(1), eNodeBs.get(4), 150);
+		Connection x7 = new Connection("E2-E5", eNodeBs.get(2), eNodeBs.get(5), 50);
+		Connection x8 = new Connection("E3-E6", eNodeBs.get(3), eNodeBs.get(6), 75);
+		Connection x9 = new Connection("E4-E7", eNodeBs.get(4), eNodeBs.get(7), 125);
+		Connection x10 = new Connection("E5-E8", eNodeBs.get(5), eNodeBs.get(8), 75);
+		Connection x11 = new Connection("E6-E9", eNodeBs.get(6), eNodeBs.get(9), 50);
+		Connection x13 = new Connection("E10-E8", eNodeBs.get(10), eNodeBs.get(8), 175);
+		Connection x15 = new Connection("E1-E2", eNodeBs.get(1), eNodeBs.get(2), 125);
+		Connection x16 = new Connection("E2-E3", eNodeBs.get(2), eNodeBs.get(3), 100);
+		Connection x17 = new Connection("E4-E5", eNodeBs.get(4), eNodeBs.get(5), 75);
+		Connection x18 = new Connection("E5-E6", eNodeBs.get(5), eNodeBs.get(6), 100);
+		Connection x19 = new Connection("E7-E8", eNodeBs.get(7), eNodeBs.get(8), 100);
+		Connection x20 = new Connection("E8-E9", eNodeBs.get(8), eNodeBs.get(9), 150);
 		
 	}
 
